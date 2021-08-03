bd779981dcd74c79ae4b11bc6e04cb8658a9306e
Michele Tagliabue <michele.tagliabue@unox.com>
2021-08-03 11:37:18 +0200

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const PrettierPlugin = require("prettier-webpack-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
-const getPackageJson = require('./scripts/getPackageJson');
+const TerserPlugin = require("terser-webpack-plugin");
+const getPackageJson = require("./scripts/getPackageJson");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const {
-  version,
-  name,
-  license,
-  repository,
-  author,
-} = getPackageJson('version', 'name', 'license', 'repository', 'author');
+const { version, name, license, repository, author } = getPackageJson(
+  "version",
+  "name",
+  "license",
+  "repository",
+  "author"
+);
 
 const banner = `
   ${name} v${version}
@@ -26,32 +26,33 @@ const banner = `
 
 module.exports = {
   mode: "production",
-  entry: './src/lib/index.ts',
-  target: 'es5',
+  entry: "./src/lib/index.ts",
+  target: "es5",
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
     library: "SimpleKeyboard",
-    libraryTarget: 'umd',
+    libraryTarget: "umd",
     clean: true,
-    globalObject: 'this',
+    globalObject: "this",
+    chunkFormat: "commonjs",
     environment: {
-      arrowFunction: false
-    }
+      arrowFunction: false,
+    },
   },
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin({ extractComments: false }),
-      new OptimizeCSSAssetsPlugin()
+      new OptimizeCSSAssetsPlugin(),
     ],
   },
   devServer: {
     open: true,
     hot: true,
     host: "localhost",
-    static: path.join(__dirname, 'demo'),
-    port: 9000
+    static: path.join(__dirname, "demo"),
+    port: 9000,
   },
   module: {
     rules: [
@@ -59,8 +60,8 @@ module.exports = {
         test: /\.m?(j|t)s$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -71,11 +72,7 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [
-                  [
-                    "autoprefixer"
-                  ],
-                ],
+                plugins: [["autoprefixer"]],
               },
             },
           },
@@ -83,18 +80,18 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
-        use: ['url-loader'],
-      }
-    ]
+        use: ["url-loader"],
+      },
+    ],
   },
   plugins: [
     new PrettierPlugin(),
     new MiniCssExtractPlugin({
-        filename: 'css/index.css'
+      filename: "css/index.css",
     }),
-    new webpack.BannerPlugin(banner)
+    new webpack.BannerPlugin(banner),
   ],
   resolve: {
-    extensions: ['.ts', '.js', '.json']
-  }
+    extensions: [".ts", ".js", ".json"],
+  },
 };
